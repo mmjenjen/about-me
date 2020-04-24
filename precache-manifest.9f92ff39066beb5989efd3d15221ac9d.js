6017256ffad35eedf57aae849d5060ee04dbf0ed
Maggie Jennings <mjennings@Maggies-MacBook-Air.local>
2020-04-24 07:53:24 -0500

    Updates

---
+++ precache-manifest.9f92ff39066beb5989efd3d15221ac9d.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd0823d583f02b3166314c22c82c7a04",
+    "url": "/about-me/index.html"
+  },
+  {
+    "revision": "e20c23143d572d46494a",
+    "url": "/about-me/static/css/main.2679f680.chunk.css"
+  },
+  {
+    "revision": "8d00ad5714a9227482ba",
+    "url": "/about-me/static/js/2.1e81a6da.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/about-me/static/js/2.1e81a6da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e20c23143d572d46494a",
+    "url": "/about-me/static/js/main.ed3b8a6d.chunk.js"
+  },
+  {
+    "revision": "99069d7c1797e5739077",
+    "url": "/about-me/static/js/runtime-main.ebc97ae4.js"
+  },
+  {
+    "revision": "a5e1e4928a318f431e2d8a3ff8d2db44",
+    "url": "/about-me/static/media/london.a5e1e492.JPG"
+  },
+  {
+    "revision": "aea2990e9c77f0e6e6b2711a5fbaea9e",
+    "url": "/about-me/static/media/paris.aea2990e.JPG"
+  },
+  {
+    "revision": "02b4b471745b48a7d2b3732ae2c0f5e3",
+    "url": "/about-me/static/media/sapporo.02b4b471.JPG"
+  },
+  {
+    "revision": "4303d6d82cfdf167335c68296732b0c7",
+    "url": "/about-me/static/media/showa2.4303d6d8.JPG"
+  }
+]);
